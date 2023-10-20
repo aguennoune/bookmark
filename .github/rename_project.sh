c648615e0f7d2526b40bc3dd7e9dcce7582eed4d
aguennoune <aguennoune@users.noreply.github.com>
2023-10-20 22:41:17 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="aguennoune"
+original_name="bookmark"
+original_urlname="bookmark"
+original_description="Awesome bookmark created by aguennoune"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv bookmark $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
